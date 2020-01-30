f242e78f02bf5bcb611225c7a2b0f9186aa4f39c
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2020-01-30 09:34:09 -0300

    finishing files from array slice map folder

---
+++ arrayslicemap/appendcopy/appendcopy.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	array1 := [3]int{1, 2, 3}
+	var slice []int
+
+	// array1 = append(array1, 4, 5, 6)
+	slice = append(slice, 4, 5, 6)
+	fmt.Println(array1, slice)
+
+	slice2 := make([]int, 2)
+	copy(slice2, slice)
+	fmt.Println(slice2)
+}
